51edeba9c8c0e395253b23a9cf44865d4306fe50
Tim Bocek <tim.bocek@gmail.com>
2011-06-19 06:48:06 +0000

    THEY ARE CIRCLES!!!!!!111
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@22 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/graphicscore/DrawableToken.java
+++ src/com/tbocek/android/combatmap/graphicscore/DrawableToken.java
@@ -3,6 +3,7 @@ package com.tbocek.android.combatmap.graphicscore;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
+import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.Rect;
@@ -34,10 +35,26 @@ public abstract class DrawableToken extends BaseToken {
 	public void draw(Canvas c, float x, float y, float radius) {
 		Drawable d = getDrawable();
 		if (d != null) {
+			c.save(Canvas.CLIP_SAVE_FLAG);
+			clipToCircle(c, x, y, radius);
 			d.setBounds(new Rect((int)(x-radius), (int)(y-radius),
 				                     (int)(x+radius), (int)(y+radius)));
 			d.draw(c);
+			c.restore();
 		}
+	}
+	
+	/**
+	 * Sets the clip of the given canvas to a circle centered at (x,y) with radius r
+	 * @param c
+	 * @param x
+	 * @param y
+	 * @param radius
+	 */
+	private void clipToCircle(Canvas c, float x, float y, float radius) {
+		Path p = new Path();
+		p.addCircle(x, y, radius, Path.Direction.CW);
+		c.clipPath(p);
 	}
 
 	@Override
