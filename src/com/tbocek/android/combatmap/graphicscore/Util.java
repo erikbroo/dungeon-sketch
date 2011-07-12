b674f2a56177498d7b68b2f955968506bc473d2e
Tim Bocek <tim.bocek@gmail.com>
2011-07-12 05:47:42 +0000

    Rate limit lines based on screen space.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@43 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/graphicscore/Util.java
+++ src/com/tbocek/android/combatmap/graphicscore/Util.java
@@ -8,7 +8,11 @@ import android.graphics.Color;
 public final class Util {
 
 	public static float distance(PointF p1, PointF p2) {
-		return (float)Math.sqrt((double)(p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y)); 
+		return distance(p1.x, p1.y, p2.x, p2.y);
+	}
+	
+	public static float distance(float x1, float y1, float x2, float y2) {
+		return (float)Math.sqrt((double)(x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)); 
 	}
 	
 	public static float degreesToRadians(float deg) {
--- src/com/tbocek/android/combatmap/view/FingerDrawInteractionMode.java
+++ src/com/tbocek/android/combatmap/view/FingerDrawInteractionMode.java
@@ -2,11 +2,13 @@ package com.tbocek.android.combatmap.view;
 
 import com.tbocek.android.combatmap.graphicscore.Line;
 import com.tbocek.android.combatmap.graphicscore.PointF;
+import com.tbocek.android.combatmap.graphicscore.Util;
 
 import android.view.MotionEvent;
 
 public class FingerDrawInteractionMode extends CombatViewInteractionMode {
-
+	private float lastPointX;
+	private float lastPointY;
 	public FingerDrawInteractionMode(CombatView view) {
 		super(view);
 	}
@@ -18,13 +20,28 @@ public class FingerDrawInteractionMode extends CombatViewInteractionMode {
 		if (currentLine == null) {
 			return true;
 		}
+		boolean DEBUG_shoulddraw = shouldDrawLine(e2.getX(), e2.getY());
+		if (shouldDrawLine(e2.getX(), e2.getY())) {
+			addLinePoint(e2);
+		}
+		return true;
+	}
+
+	private void addLinePoint(MotionEvent e2) {
 		currentLine.addPoint(view.getTransformer().screenSpaceToWorldSpace(new PointF(e2.getX(), e2.getY())));
 		view.invalidate();
-		return true;
+		lastPointX = e2.getX();
+		lastPointY = e2.getY();
+	}
+	
+	private boolean shouldDrawLine(float newPointX, float newPointY) {
+		return Util.distance(lastPointX, lastPointY, newPointX, newPointY) > 3;
 	}
 	
 	public boolean onDown(MotionEvent e) {
 		currentLine = view.createLine();
+		lastPointX = e.getX();
+		lastPointY = e.getY();
 		return true;
 	}
 }
