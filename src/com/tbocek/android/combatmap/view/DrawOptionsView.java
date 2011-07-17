001fdb9ac293355bf922c1a367899fd9561ae42d
Tim Bocek <tim.bocek@gmail.com>
2011-07-17 17:06:12 +0000

    Fixstyle changes.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@57 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/view/DrawOptionsView.java
+++ src/com/tbocek/android/combatmap/view/DrawOptionsView.java
@@ -7,57 +7,123 @@ import com.tbocek.android.combatmap.R;
 import com.tbocek.android.combatmap.graphicscore.Util;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.view.View;
-import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-public class DrawOptionsView extends HorizontalScrollView {
+public final class DrawOptionsView extends HorizontalScrollView {
 
-	LinearLayout layout;
+	/**
+	 * The width on each side of the buttons used to pick a new color.
+	 */
+	private static final int COLOR_BUTTON_SIZE = 48;
 	
-	List<ImageToggleButton> toolsGroup = new ArrayList<ImageToggleButton>();
-	List<ImageToggleButton> colorGroup = new ArrayList<ImageToggleButton>();
+	/**
+	 * The width of the space that seperates tools and colors.
+	 */
+	private static final int SEPERATOR_WIDTH = 20;
 	
+	/**
+	 * The layout that will hold drawing buttons.
+	 */
+	private LinearLayout layout;
+	
+	/**
+	 * A list of all buttons that select a drawing tool, so that they can be
+	 * modified as a group.
+	 */
+	private List<ImageToggleButton> toolsGroup =
+		new ArrayList<ImageToggleButton>();
+	
+	/**
+	 * A list of all buttons that select a color, so that they can be modified
+	 * as a group.
+	 */
+	private List<ImageToggleButton> colorGroup =
+		new ArrayList<ImageToggleButton>();
+	
+	/**
+	 * OnClickListener for when a button representing a color is clicked.
+	 * @author Tim Bocek
+	 */
 	private class ColorListener implements View.OnClickListener {
-		private int color;
-		public ColorListener(int color) {
-			this.color = color;
+		/**
+		 * The color that this listener will pick when fired.
+		 */
+		private int mColor;
+		public ColorListener(final int color) {
+			this.mColor = color;
 		}
 		
 		@Override
-		public void onClick(View v) {
-			onChangeDrawToolListener.onChooseColoredPen(color);
+		public void onClick(final View v) {
+			onChangeDrawToolListener.onChooseColoredPen(mColor);
 			untoggleGroup(colorGroup);
-			((ImageToggleButton)v).setToggled(true);
+			((ImageToggleButton) v).setToggled(true);
 		}
 	}
 	
+	/**
+	 * OnClickListener for when a button representing a drawing width is
+	 * clicked.
+	 * @author Tim Bocek
+	 */
 	private class StrokeWidthListener implements View.OnClickListener {
-		private int width;
-		public StrokeWidthListener(int width) {
-			this.width = width;
+		/**
+		 * The line width that the drawing tool will be changed to when this
+		 * listener fires.
+		 */
+		private int mWidth;
+		public StrokeWidthListener(final int width) {
+			this.mWidth = width;
 		}
 		
 		@Override
-		public void onClick(View v) {
-			onChangeDrawToolListener.onChooseStrokeWidth(width);	
+		public void onClick(final View v) {
+			onChangeDrawToolListener.onChooseStrokeWidth(mWidth);	
 			untoggleGroup(toolsGroup);
 			setGroupVisibility(colorGroup, View.VISIBLE);
-			((ImageToggleButton)v).setToggled(true);
+			((ImageToggleButton) v).setToggled(true);
 		}
 	}
 	
-	
+	/**
+	 * Listener that is called when different drawing tools are chosen.
+	 * @author Tim Bocek
+	 */
 	public interface OnChangeDrawToolListener {
+		/**
+		 * Fired when the eraser tool is selected.
+		 */
 		void onChooseEraser();
+		
+		/**
+		 * Fired when the color is changed.
+		 * @param color The new color.
+		 */
 		void onChooseColoredPen(int color);
+		
+		/**
+		 * Fired when the pan tool is selected.
+		 */
 		void onChoosePanTool();
+		
+		/**
+		 * Fired when the stroke width is changed.  This can also be thought of
+		 * as selecting a pen tool with the given stroke width.
+		 * @param width The new stroke width.
+		 */
 		void onChooseStrokeWidth(int width);
 	}
 	
+	/**
+	 * Using the null class pattern; this implementation of
+	 * OnChangeDrawToolListener should do nothing.  It is only here to avoid
+	 * null checkes whenever one of these methods is called.
+	 * @author Tim
+	 *
+	 */
 	public class NullChangeDrawToolListener implements OnChangeDrawToolListener {
 		@Override
 		public void onChooseEraser() {}
@@ -72,9 +138,18 @@ public class DrawOptionsView extends HorizontalScrollView {
 		public void onChooseStrokeWidth(int width) {}
 	}
 	
-	private OnChangeDrawToolListener onChangeDrawToolListener = new NullChangeDrawToolListener();
+	/**
+	 * The listener that is called when something about the current draw tool
+	 * changes.
+	 */
+	private OnChangeDrawToolListener onChangeDrawToolListener =
+		new NullChangeDrawToolListener();
 	
-	public DrawOptionsView(Context context) {
+	/**
+	 * Constructs a new DrawOptionsView.
+	 * @param context The context to construct in.
+	 */
+	public DrawOptionsView(final Context context) {
 		super(context);
 		layout = new LinearLayout(context);
 		addView(layout);
@@ -88,7 +163,7 @@ public class DrawOptionsView extends HorizontalScrollView {
 		
 		panButton.setOnClickListener(new View.OnClickListener() {	
 			@Override
-			public void onClick(View arg0) {
+			public void onClick(final View v) {
 				onChangeDrawToolListener.onChoosePanTool();
 				untoggleGroup(toolsGroup);
 				setGroupVisibility(colorGroup, View.GONE);
@@ -98,7 +173,7 @@ public class DrawOptionsView extends HorizontalScrollView {
 		
 		eraserButton.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View arg0) {
+			public void onClick(final View v) {
 				onChangeDrawToolListener.onChooseEraser();
 				untoggleGroup(toolsGroup);
 				setGroupVisibility(colorGroup, View.GONE);
@@ -118,7 +193,10 @@ public class DrawOptionsView extends HorizontalScrollView {
 		
 		//Create a seperator
 		ImageView seperator = new ImageView(this.getContext());
-		seperator.setLayoutParams(new LinearLayout.LayoutParams(20, LinearLayout.LayoutParams.MATCH_PARENT));
+		seperator.setLayoutParams(
+				new LinearLayout.LayoutParams(
+						SEPERATOR_WIDTH,
+						LinearLayout.LayoutParams.MATCH_PARENT));
 		layout.addView(seperator);
 		
 		for (int color : Util.getStandardColorPalette()) {
@@ -126,15 +204,26 @@ public class DrawOptionsView extends HorizontalScrollView {
 		}
 	}
 	
-	private void addColorButton(int color) {
+	/**
+	 * Adds a button that selects the given color as the current color.
+	 * @param color The color that this button will select.
+	 */
+	private void addColorButton(final int color) {
 		PencilButton b = new PencilButton(this.getContext(), color);
 		b.setOnClickListener(new ColorListener(color));
-		b.setLayoutParams(new LinearLayout.LayoutParams(48,48));
+		b.setLayoutParams(new LinearLayout.LayoutParams(
+				COLOR_BUTTON_SIZE, COLOR_BUTTON_SIZE));
 		layout.addView(b);
 		colorGroup.add(b);
 	}
 	
-	private void addStrokeWidthButton(int width, int resourceId) {
+	/**
+	 * Adds a button that changes the stroke width to the given width, and that
+	 * uses the given resource ID to represent its self.
+	 * @param width The width that this button will change the stroke width to.
+	 * @param resourceId ID of the image to draw on this button.
+	 */
+	private void addStrokeWidthButton(final int width, final int resourceId) {
 		ImageToggleButton b = new ImageToggleButton(this.getContext());
 		b.setImageResource(resourceId);
 		b.setOnClickListener(new StrokeWidthListener(width));
@@ -142,12 +231,22 @@ public class DrawOptionsView extends HorizontalScrollView {
 		toolsGroup.add(b);
 	}
 
-	public void setOnChangeDrawToolListener(OnChangeDrawToolListener onChangeDrawToolListener) {
-		this.onChangeDrawToolListener = onChangeDrawToolListener;
+	/**
+	 * Sets the listener to call when a new draw tool is selected.
+	 * @param listener The new listener
+	 */
+	public void setOnChangeDrawToolListener(
+			final OnChangeDrawToolListener listener) {
+		this.onChangeDrawToolListener = listener;
 	}
 	
-	private void untoggleGroup(List<ImageToggleButton> group) {
-		for (ImageToggleButton b:group) {
+	/**
+	 * Untoggles an entire list of ImageToggleButtons, so we can make sure that
+	 * only one button in the list is toggled at once.
+	 * @param group The list of ImageToggleButtons to modify.
+	 */
+	private void untoggleGroup(final List<ImageToggleButton> group) {
+		for (ImageToggleButton b : group) {
 			b.setToggled(false);
 		}
 	}
