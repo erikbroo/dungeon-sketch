1e3eb8447da438328cb64c8ace4310071c3bcb36
Tim Bocek <tim.bocek@gmail.com>
2011-07-17 23:57:18 +0000

    Checkstyle fixes.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@72 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/Settings.java
+++ src/com/tbocek/android/combatmap/Settings.java
@@ -3,9 +3,14 @@ package com.tbocek.android.combatmap;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
-public class Settings extends PreferenceActivity {
+/**
+ * Preferences activity for Dungeon Sketch.
+ * @author Tim
+ *
+ */
+public final class Settings extends PreferenceActivity {
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.layout.settings);
 	}
