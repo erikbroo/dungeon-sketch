c07296dacb7bb0db86979f2c37393727d4f24144
Tim Bocek <tim.bocek@gmail.com>
2011-07-17 23:56:05 +0000

    Checkstyle fixes.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@71 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/TextPromptDialog.java
+++ src/com/tbocek/android/combatmap/TextPromptDialog.java
@@ -6,20 +6,61 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+/**
+ * An Android dialog that allows the user to enter text.  Provides a text entry
+ * field, and a confirm button.  The system back button is used to cancel.
+ * Allows customization of the dialog title and the text displayed on the
+ * confirmation button.
+ * @author Tim Bocek
+ *
+ */
 public class TextPromptDialog extends Dialog {
+	
+	/**
+	 * Listener used to specify the action to take when the user confirms text
+	 * entry in a TextPromptDialog.
+	 * @author Tim Bocek
+	 *
+	 */
 	public interface OnTextConfirmedListener {
-		public void onTextConfirmed(String text);
+		/**
+		 * Called when the user confirms the text entered.
+		 * @param text The text entered by the user.
+		 */
+		void onTextConfirmed(String text);
 	}
 	
-	TextView nameText;
-	Button confirmButton;
-	OnTextConfirmedListener listener;
 	
-	public TextPromptDialog(Context context, OnTextConfirmedListener listener, String title, String confirmText) {
+	/**
+	 * Text entry field.
+	 */
+	private TextView nameText;
+	
+	/**
+	 * Button that the user clicks to confirm the text entered.
+	 */
+	private Button confirmButton;
+	
+	/**
+	 * Listener that is called when the user clicks the confirm button.
+	 */
+	private OnTextConfirmedListener mListener;
+	
+	/**
+	 * Constructor.
+	 * @param context Context to create the dialog in.
+	 * @param listener Listener that specifies the action to take when the user
+	 * 		confirms the text entered.
+	 * @param title Title to display in the dialog.
+	 * @param confirmText Text to display on the confirmation button.
+	 */
+	public TextPromptDialog(
+			final Context context, final OnTextConfirmedListener listener, 
+			final String title, final String confirmText) {
 		super(context);
 		this.setContentView(R.layout.save);
 		this.setTitle(title);
-		this.listener = listener;
+		this.mListener = listener;
 		
 		confirmButton = (Button) this.findViewById(R.id.button_save);
 		confirmButton.setText(confirmText);
@@ -29,15 +70,19 @@ public class TextPromptDialog extends Dialog {
 		
 		confirmButton.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View arg0) {
+			public void onClick(final View v) {
 				String name = (String) nameText.getText().toString();
 				dismiss();
-				TextPromptDialog.this.listener.onTextConfirmed(name);
+				TextPromptDialog.this.mListener.onTextConfirmed(name);
 			}
 		});
 	}
 	
-	protected void onResume() {
+	/**
+	 * On resuming the activity, clears the text so that nothing is left over
+	 * from the last time the dialog was opened.
+	 */
+	protected final void onResume() {
 		nameText.setText("");
 	}
 
