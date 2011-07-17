0b88471837bb0ed654206b63d4b53ce2eebd7cd3
Tim Bocek <tim.bocek@gmail.com>
2011-07-17 23:49:49 +0000

    Checkstyle fixes.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@70 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/graphicscore/PointF.java
+++ src/com/tbocek/android/combatmap/graphicscore/PointF.java
@@ -2,23 +2,52 @@ package com.tbocek.android.combatmap.graphicscore;
 import java.io.IOException;
 import java.io.Serializable;
 
-// Makes Android's PointF serializable
-public class PointF extends android.graphics.PointF implements Serializable{
+/**
+ * Serializable version of Android's PointF.
+ * @author Tim Bocek
+ *
+ */
+public class PointF extends android.graphics.PointF implements Serializable {
+	
+	/**
+	 * ID for serialization.
+	 */
 	private static final long serialVersionUID = -8072053114782117778L;
+	
+	/**
+	 * Default constructor.
+	 */
 	public PointF() {
 		super();
 	}
 	
-	public PointF(float x, float y) {
+	/**
+	 * Constructor from coordinates.
+	 * @param x X coordinate.
+	 * @param y Y coordinate.
+	 */
+	public PointF(final float x, final float y) {
 		super(x, y);
 	}
 	
-	private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+	/**
+	 * Serializes the PointF.
+	 * @param out Stream to serialize to.
+	 * @throws IOException On output error.
+	 */
+	private void writeObject(final java.io.ObjectOutputStream out)
+			throws IOException {
 		out.writeFloat(x);
 		out.writeFloat(y);
 	}
 
-	private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+	/**
+	 * Deserializes the PointF.
+	 * @param in Stream to deserialize from.
+	 * @throws IOException On input error.
+	 */
+	private void readObject(final java.io.ObjectInputStream in) 
+			throws IOException {
 		x = in.readFloat();
 		y = in.readFloat();
 	}
