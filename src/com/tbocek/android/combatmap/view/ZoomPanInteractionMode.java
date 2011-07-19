355a6980ae7ab841da4dd1f2b0d5df6a11074431
Tim Bocek <tim.bocek@gmail.com>
2011-07-19 17:32:36 +0000

    Style fixes.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@78 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/view/ZoomPanInteractionMode.java
+++ src/com/tbocek/android/combatmap/view/ZoomPanInteractionMode.java
@@ -3,18 +3,29 @@ package com.tbocek.android.combatmap.view;
 
 import android.view.MotionEvent;
 
+
+/**
+ * Extends the default combat view gesture behavior to allow a single finger to
+ * scroll around the map area.
+ *
+ * @author Tim Bocek
+ */
 public class ZoomPanInteractionMode extends CombatViewInteractionMode {
-	public ZoomPanInteractionMode(CombatView view) {
-		super(view);
-	}
+    /**
+     * Constructor
+     *
+     * @param view The CombatView that this interaction mode interacts with.
+     */
+	  public ZoomPanInteractionMode(CombatView view) {
+		  super(view);
+	  }
 	
     @Override
-	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-    	view.getTransformer().moveOrigin(-distanceX, -distanceY);
-    	view.invalidate();
-		return true;
-	}
-    
-    
-
+	  public boolean onScroll(
+            MotionEvent e1, MotionEvent e2, 
+            float distanceX, float distanceY) {
+    	  view.getTransformer().moveOrigin(-distanceX, -distanceY);
+    	  view.invalidate();
+        return true;
+	  }
 }
