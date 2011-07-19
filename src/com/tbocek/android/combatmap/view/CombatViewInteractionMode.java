401bc684498a8ad03f15da2ee4133b2c7f26a915
Tim Bocek <tim.bocek@gmail.com>
2011-07-19 18:03:12 +0000

    tabs->spaces.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@81 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/view/CombatViewInteractionMode.java
+++ src/com/tbocek/android/combatmap/view/CombatViewInteractionMode.java
@@ -19,57 +19,55 @@ import android.view.ScaleGestureDetector;
  */
 public class CombatViewInteractionMode extends SimpleAllGestureListener {
 
-  /**
-   * The CombatView that this interaction mode manipulates.
-   */
-	protected CombatView view;
+    /**
+     * The CombatView that this interaction mode manipulates.
+     */
+    protected CombatView view;
 
-  /**
-   * Constructor
-   * @param view The CombatView that this interaction mode manipulates.
-   */
-	public CombatViewInteractionMode(CombatView view) {
-		this.view = view;
-	}
+    /**
+     * Constructor
+     * @param view The CombatView that this interaction mode manipulates.
+     */
+	  public CombatViewInteractionMode(CombatView view) {
+	      this.view = view;
+	  }
 
-	@Override
-	public boolean onScale(ScaleGestureDetector detector) {
-     view.getTransformer().zoom(
-       detector.getScaleFactor(),
-       new PointF(detector.getFocusX(), detector.getFocusY()));
-     view.invalidate();
-     return true;
-	}
+	  @Override
+	  public boolean onScale(ScaleGestureDetector detector) {
+        view.getTransformer().zoom(
+            detector.getScaleFactor(),
+            new PointF(detector.getFocusX(), detector.getFocusY()));
+        view.invalidate();
+        return true;
+	  }
 
-  /**
-   * Allows the interaction mode to specify custom context menu options.
-   * @param menu The context menu to populate
-   */
-	public void onCreateContextMenu(ContextMenu menu) {
-	}
+    /**
+     * Allows the interaction mode to specify custom context menu options.
+     * @param menu The context menu to populate
+     */
+	  public void onCreateContextMenu(ContextMenu menu) {
+	  }
 
-  /**
-   * Allows the interaction mode to specify the actions taken in
-   * response to context menu items that it added.
-   * @param item The context menu item clicked.
-   */
-	public boolean onContextItemSelected(MenuItem item) {
-		return false;
-	}
+    /**
+     * Allows the interaction mode to specify the actions taken in
+     * response to context menu items that it added.
+     * @param item The context menu item clicked.
+     */
+	  public boolean onContextItemSelected(MenuItem item) {
+	      return false;
+    }
 	
-	/**
-	 * Allows the manipulation mode to draw custom user interface elements.
-	 * @param c The canvas to draw on.
-	 */
-	public void draw(Canvas c) {
-		
-	}
+	  /**
+	   * Allows the manipulation mode to draw custom user interface elements.
+	   * @param c The canvas to draw on.
+	   */
+	  public void draw(Canvas c) {
+		}
 	
-  /**
-   * Action to take when a finger is lifted.
-   * @param event Event info.
-   */
-	public void onUp(MotionEvent event) {
-		
-	}
+    /**
+     * Action to take when a finger is lifted.
+     * @param event Event info.
+     */
+	  public void onUp(MotionEvent event) {
+	  }
 }
