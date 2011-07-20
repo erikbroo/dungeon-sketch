60e786296b1678fe6056496e8a557bb109cdfca3
Tim Bocek <tim.bocek@gmail.com>
2011-07-20 07:09:56 +0000

    some checkstyle fixes.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@88 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/view/GridRepositioningInteractionMode.java
+++ src/com/tbocek/android/combatmap/view/GridRepositioningInteractionMode.java
@@ -5,14 +5,16 @@ import com.tbocek.android.combatmap.graphicscore.PointF;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 
-public final class GridRepositioningInteractionMode extends CombatViewInteractionMode {
+public final class GridRepositioningInteractionMode
+		extends CombatViewInteractionMode {
 
     public GridRepositioningInteractionMode(CombatView view) {
         super(view);
     }
 
     @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+    public boolean onScroll(
+    		MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
         view.getData().grid.gridSpaceToWorldSpaceTransformer().moveOrigin(
                 -view.getTransformer().screenSpaceToWorldSpace(distanceX),
                 -view.getTransformer().screenSpaceToWorldSpace(distanceY));
@@ -22,8 +24,11 @@ public final class GridRepositioningInteractionMode extends CombatViewInteractio
 
     @Override
     public boolean onScale(ScaleGestureDetector detector) {
-        PointF invariantPointWorldSpace = view.getTransformer().screenSpaceToWorldSpace(detector.getFocusX(), detector.getFocusY());
-        view.getData().grid.gridSpaceToWorldSpaceTransformer().zoom(detector.getScaleFactor(), invariantPointWorldSpace);
+        PointF invariantPointWorldSpace =
+        	view.getTransformer().screenSpaceToWorldSpace(
+        			detector.getFocusX(), detector.getFocusY());
+        view.getData().grid.gridSpaceToWorldSpaceTransformer().zoom(
+        		detector.getScaleFactor(), invariantPointWorldSpace);
         view.invalidate();
         return true;
     }
