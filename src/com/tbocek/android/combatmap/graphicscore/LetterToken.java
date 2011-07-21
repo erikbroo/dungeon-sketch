87282f6a29a97b73b73aedf2b22a34c41ac4befc
Tim Bocek <tim.bocek@gmail.com>
2011-07-21 00:04:32 +0000

    Commenting and other checkstyle fixes.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@92 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/graphicscore/LetterToken.java
+++ src/com/tbocek/android/combatmap/graphicscore/LetterToken.java
@@ -8,10 +8,26 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 
+/**
+ * This class represents a token that draws as a capital letter
+ * inside a circle.
+ * 
+ * @author Tim Bocek
+ *
+ */
 public final class LetterToken extends BaseToken {
+
+    /**
+     * The letter to draw in the circle.  While this could be anything, it
+     * should really only be a single character.
+     */
     private String letter;
 
-    public LetterToken(String letter) {
+    /**
+     * Constructor.
+     * @param letter The single character to draw in the circle.
+     */
+    public LetterToken(final String letter) {
         this.letter = letter;
     }
 
@@ -21,20 +37,28 @@ public final class LetterToken extends BaseToken {
     }
 
     @Override
-    public void drawBloodied(Canvas c, float x, float y, float radius) {
+    public void drawBloodied(final Canvas c, final float x, final float y, final float radius) {
         Paint p = new Paint();
         p.setColor(Color.RED);
         draw(c, x, y, radius, p);
     }
 
     @Override
-    public void draw(Canvas c, float x, float y, float radius) {
+    public void draw(final Canvas c, final float x, final float y, final float radius) {
         Paint p = new Paint();
         p.setColor(Color.BLACK);
         draw(c, x, y, radius, p);
     }
 
-    private void draw(Canvas c, float x, float y, float radius, Paint paint) {
+    /**
+     * Draws the token with the given paint style.
+     * @param c Canvas to draw on.
+     * @param x X coordinate of the token center, in screen space.
+     * @param y Y coordinate of the token center, in screen space.
+     * @param radius Radius of the token, in screen space.
+     * @param paint Paint object to use when drawing the circle and text.
+     */
+    private void draw(final Canvas c, final float x, final float y, final float radius, final Paint paint) {
         paint.setStrokeWidth(3);
         paint.setStyle(Style.STROKE);
         c.drawCircle(x, y, radius, paint);
@@ -45,7 +69,7 @@ public final class LetterToken extends BaseToken {
     }
 
     @Override
-    public void drawGhost(Canvas c, float x, float y, float radius) {
+    public void drawGhost(final Canvas c, final float x, final float y, final float radius) {
         //TODO(tim.bocek): Make this look different
         Paint p = new Paint();
         p.setColor(Color.GRAY);
@@ -56,7 +80,8 @@ public final class LetterToken extends BaseToken {
     protected String getTokenClassSpecificId() {
         return letter;
     }
-
+    
+    @Override
     public Set<String> getDefaultTags() {
         Set<String> s = new HashSet<String>();
         s.add("built-in");
