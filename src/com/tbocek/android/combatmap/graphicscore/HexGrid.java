a67cea0fbb4fe24b527d3a50fda9057e1ead9836
Tim Bocek <tim.bocek@gmail.com>
2011-07-21 01:16:54 +0000

    Checkstyle fixes.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@94 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/graphicscore/HexGrid.java
+++ src/com/tbocek/android/combatmap/graphicscore/HexGrid.java
@@ -14,33 +14,33 @@ public final class HexGrid extends Grid {
     /**
      * Cached result for the tan(30 degrees).
      */
-    private static float TANGENT_30_DEGREES =
+    private static final float TANGENT_30_DEGREES =
         (float) Math.tan(30 * Math.PI / 180);
 
     @Override
     public PointF getNearestSnapPoint(final PointF currentLocation,
             final float tokenDiameter) {
         float h = .5f * TANGENT_30_DEGREES;
-        float previousGridLineX = 
-            (float) Math.floor((double) currentLocation.x) + h/2;
-        float previousGridLineY = 
+        float previousGridLineX =
+            (float) Math.floor((double) currentLocation.x) + h / 2;
+        float previousGridLineY =
             (float) Math.floor((double) currentLocation.y);
 
-        if (((int)previousGridLineX) % 2 == 0) {
+        if (((int) previousGridLineX) % 2 == 0) {
             previousGridLineY -= .5;
         }
 
         float offset =
             .5f * tokenDiameter - (float)Math.floor(.5 * tokenDiameter);
 
-        // If we have a token that is smaller than one grid line, find the 
+        // If we have a token that is smaller than one grid line, find the
         // nearest subgrid line instead.
         if (tokenDiameter < 1) {
-            previousGridLineX += 
-                (currentLocation.x - previousGridLineX) 
+            previousGridLineX +=
+                (currentLocation.x - previousGridLineX)
                 - (currentLocation.x - previousGridLineX) % tokenDiameter;
-            previousGridLineY += 
-                (currentLocation.y - previousGridLineY) 
+            previousGridLineY +=
+                (currentLocation.y - previousGridLineY)
                 - (currentLocation.y - previousGridLineY) % tokenDiameter;
         }
 
@@ -61,9 +61,9 @@ public final class HexGrid extends Grid {
         int height = canvas.getHeight();
 
         float squareSize = transformer.worldSpaceToScreenSpace(1.0f);
-        float numSquaresHorizontal = (float)width/squareSize;
-        float numSquaresVertical = 
-            (numSquaresHorizontal * ((float)height)/((float)width));
+        float numSquaresHorizontal = (float) width / squareSize;
+        float numSquaresVertical =
+            (numSquaresHorizontal * ((float) height) / ((float) width));
 
         PointF origin = transformer.getOrigin();
 
@@ -71,26 +71,25 @@ public final class HexGrid extends Grid {
         float offsetY = origin.y % squareSize;
 
         int innerPointStartX =
-            (int)((origin.x % (squareSize * 2)) / squareSize);
-        int innerPointStartY =
-            (int)((origin.y % (squareSize * 2)) / squareSize);
+            (int) ((origin.x % (squareSize * 2)) / squareSize);
+
 
         float h = .5f * squareSize * TANGENT_30_DEGREES;
 
         // Draw the vertical undulating "lines".  We want to start slightly
         // offscreen
         for (int j = -1; j <= numSquaresHorizontal + 1; ++j) {
-            float x = j*squareSize + offsetX;
+            float x = j * squareSize + offsetX;
             float innerX = x + h;
             for (int i = -1; i <= numSquaresVertical + 1; ++i) {
-                float y = i * squareSize + offsetY 
-                    - (j-innerPointStartX) % 2 *.5f * squareSize;
+                float y = i * squareSize + offsetY
+                    - (j - innerPointStartX) % 2 * .5f * squareSize;
                 canvas.drawLine(x, y, innerX, y + squareSize / 2, paint);
                 canvas.drawLine(
                     innerX, y + squareSize / 2, x, y + squareSize, paint);
                 canvas.drawLine(
-                    innerX, y + squareSize / 2, innerX + squareSize-h,
-                    y + squareSize / 2, paint );
+                    innerX, y + squareSize / 2, innerX + squareSize - h,
+                    y + squareSize / 2, paint);
             }
         }
     }
