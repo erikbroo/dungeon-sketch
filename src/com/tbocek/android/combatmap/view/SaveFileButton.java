d41fa09cf58a83e4bb8d16e62e884304e4ba0fbe
Tim Bocek <tim.bocek@gmail.com>
2011-07-22 02:16:37 +0000

    Checkstyle fixes.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@107 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/view/SaveFileButton.java
+++ src/com/tbocek/android/combatmap/view/SaveFileButton.java
@@ -1,49 +1,79 @@
 package com.tbocek.android.combatmap.view;
 
-import com.tbocek.android.combatmap.R;
-
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public final class SaveFileButton extends LinearLayout{
-    ImageView preview;
-    TextView text;
+import com.tbocek.android.combatmap.R;
 
-    public SaveFileButton(Context context) {
+/**
+ * A button that displays the name and preview of a save file.
+ * @author Tim Bocek
+ *
+ */
+public final class SaveFileButton extends LinearLayout {
+
+	/**
+	 * The displayed preview.
+	 */
+    private ImageView preview;
+
+    /**
+     * The displayed file name.
+     */
+    private TextView text;
+
+    /**
+     * Constructor.
+     * @param context The context to create this view in.
+     */
+    public SaveFileButton(final Context context) {
         super(context);
         LayoutInflater.from(context).inflate(R.layout.saved_map_file, this);
         preview = (ImageView) findViewById(R.id.saved_map_preview);
         text = (TextView) findViewById(R.id.saved_map_file_name);
+
+        // Clicking on the preview should count as clicking on the button its
+        // self.
         preview.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View arg0) {
+            public void onClick(final View arg0) {
                 performClick();
             }
         });
     }
 
-    public void setFileName(String name) {
+    /**
+     * Sets the name of the save file.
+     * @param name The name to display.
+     */
+    public void setFileName(final String name) {
         text.setText(name);
     }
 
-    public void setPreviewImage(Bitmap image) {
+    /**
+     * Sets the image to display as the preview.
+     * @param image The image to display.
+     */
+    public void setPreviewImage(final Bitmap image) {
         preview.setImageBitmap(image);
     }
 
+    /**
+     * Returns the filename associated with this button.
+     * @return The filename.
+     */
     public String getFileName() {
         return text.getText().toString();
     }
 
     @Override
-    public void setOnCreateContextMenuListener(View.OnCreateContextMenuListener l) {
+    public void setOnCreateContextMenuListener(
+    		final View.OnCreateContextMenuListener l) {
         super.setOnCreateContextMenuListener(l);
         preview.setOnCreateContextMenuListener(l);
         text.setOnCreateContextMenuListener(l);
