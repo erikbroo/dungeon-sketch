bda19fa56cfba27f4eb7618d7501c5892c5fb16f
Tim Bocek <tim.bocek@gmail.com>
2011-07-22 02:11:19 +0000

    Checkstyle fixes.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@105 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/TokenDatabase.java
+++ src/com/tbocek/android/combatmap/TokenDatabase.java
@@ -1,21 +1,12 @@
 package com.tbocek.android.combatmap;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -27,7 +18,6 @@ import java.util.Map;
 import java.util.Set;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.widget.Toast;
 
 import com.tbocek.android.combatmap.graphicscore.BaseToken;
@@ -38,36 +28,42 @@ import com.tbocek.android.combatmap.graphicscore.SolidColorToken;
 import com.tbocek.android.combatmap.graphicscore.Util;
 
 
-public final class TokenDatabase implements Serializable {
+/**
+ * Provides a lightweight database storing a list of tokens and allowing them
+ * to be associated with tags.
+ * @author Tim
+ *
+ */
+public final class TokenDatabase {
     /**
      * Mapping from a string representing a token ID to a set of tags that that
      * token has.
      */
-    public Map<String, Set<String>> tokensForTag
+    private Map<String, Set<String>> tokensForTag
             = new HashMap<String, Set<String>>();
-            
+
     /**
      * Mapping from a string representing a tag to a set of token IDs that have
      * that tag.
      */
-    public Map<String, Set<String>> tagsForToken
+    private Map<String, Set<String>> tagsForToken
             = new HashMap<String, Set<String>>();
-            
+
     /**
      * Mapping from a Token ID to an instantiated token object that has that ID.
      */
-    public transient Map<String, BaseToken> tokenForId
+    private transient Map<String, BaseToken> tokenForId
             = new HashMap<String, BaseToken>();
 
     /**
      * Delimiter to use when saving the token database.
      */
-    private static final String FILE_DELIMITER="`";
+    private static final String FILE_DELIMITER = "`";
 
     /**
      * Private constructor - this is a singleton.
      */
-    private TokenDatabase() {}
+    private TokenDatabase() { }
 
     /**
      * The singleton token database instance.
@@ -79,46 +75,37 @@ public final class TokenDatabase implements Serializable {
      * @param context A context to use when loading data if needed.
      * @return The token database.
      */
-    public static TokenDatabase getInstance(Context context) {
+    public static TokenDatabase getInstance(final Context context) {
         if (instance == null) {
             try {
                 instance = TokenDatabase.load(context);
             } catch (Exception e) {
                 instance = new TokenDatabase();
                 instance.populate(new DataManager(context));
-                Toast t = Toast.makeText(context, 
-                        "Could not open the token database: " + e.toString(), 
+                Toast t = Toast.makeText(context,
+                        "Could not open the token database: " + e.toString(),
                         Toast.LENGTH_LONG);
+                t.show();
             }
         }
         return instance;
     }
 
     /**
-     * Adds a token to the database, signaling its availability to link to a 
-     * token ID.  There may be tokens represented in the database that are not 
-     * attached to an actual token.  These are not considered valid.
-     * @param token The token to add.
-     */
-    public void addToken(BaseToken token) {
-        tokenForId.put(token.getTokenId(), token);
-    }
-    
-    /**
      * Adds a tag with no tokens associated with it to the token database.
      * @param tag The tag to add.
      */
-    public void addEmptyTag(String tag) {
+    public void addEmptyTag(final String tag) {
         tokensForTag.put(tag, new HashSet<String>());
     }
-    
+
     /**
      * Tags the token with the given collection of tags.
      * @param tokenId ID of the token to tag.
      * @param tags Tags to add to the token.
      */
-    public void tagToken(String tokenId, Collection<String> tags) {
-        for (String tag: tags) {
+    public void tagToken(final String tokenId, final Collection<String> tags) {
+        for (String tag : tags) {
             if (!tokensForTag.containsKey(tag)) {
                 tokensForTag.put(tag, new HashSet<String>());
             }
@@ -133,19 +120,19 @@ public final class TokenDatabase implements Serializable {
 
     /**
      * Tags the token with the given collection of tags.
-     * @param token  The token object to tag.
+     * @param token The token object to tag.
      * @param tags Tags to add to the token.
      */
-    public void tagToken(BaseToken token, Set<String> tags) {
+    public void tagToken(final BaseToken token, final Set<String> tags) {
         tagToken(token.getTokenId(), tags);
     }
 
     /**
      * Tags the token with the given single tag.
-     * @param tokenID The ID of the token object to tag.
+     * @param tokenId The ID of the token object to tag.
      * @param tag The tag to add.
      */
-    public void tagToken(String tokenId, String tag) {
+    public void tagToken(final String tokenId, final String tag) {
         Set<String> tags = new HashSet<String>();
         tags.add(tag);
         tagToken(tokenId, tags);
@@ -161,28 +148,29 @@ public final class TokenDatabase implements Serializable {
         ArrayList<String> l = new ArrayList<String>(tokensForTag.keySet());
         Collections.sort(l, new Comparator<String>() {
             @Override
-            public int compare(String s1, String s2) {
+            public int compare(final String s1, final String s2) {
                 return s1.toUpperCase().compareTo(s2.toUpperCase());
             }
         });
         return l;
     }
-    
+
     /**
      * Gets all tokens in the collection, sorted by ID.
+     * @return The tokens.
      */
     public Collection<BaseToken> getAllTokens() {
         //TODO: De-dupe
-        ArrayList<String> sortedIds = 
+        ArrayList<String> sortedIds =
             new ArrayList<String>(tokenForId.keySet());
         Collections.sort(sortedIds);
 
         List<BaseToken> tokens = new ArrayList<BaseToken>();
 
-        for(String tokenId : sortedIds) {
+        for (String tokenId : sortedIds) {
             // Add the token for this ID.
             // No worries if the token doesn't exist - by design the database
-            // could include tokens that don't exist anymore since it connects a 
+            // could include tokens that don't exist anymore since it connects a
             // loaded token id to stored information about that ID.
             if (tokenForId.containsKey(tokenId)) {
                 tokens.add(tokenForId.get(tokenId));
@@ -195,10 +183,11 @@ public final class TokenDatabase implements Serializable {
      * Given a collection of tags, returns a sorted list of all tokens that have
      * at least one of those tags.
      * @param tags The tags to look for.
+     * @return The tokens for those tags.
      */
-    public List<BaseToken> tokensForTags(Collection<String> tags) {
+    public List<BaseToken> tokensForTags(final Collection<String> tags) {
         Set<String> tokenIds = new HashSet<String>();
-        for(String tag : tags) {
+        for (String tag : tags) {
             tokenIds.addAll(this.tokensForTag.get(tag));
         }
         ArrayList<String> sortedIds = new ArrayList<String>(tokenIds);
@@ -206,81 +195,81 @@ public final class TokenDatabase implements Serializable {
 
         List<BaseToken> tokens = new ArrayList<BaseToken>();
 
-        for(String tokenId : sortedIds) {
+        for (String tokenId : sortedIds) {
             // Add the token for this ID.
             // No worries if the token doesn't exist - by design the database
-            // could include tokens that don't exist anymore since it connects a loaded
-            // token id to stored information about that ID.
+            // could include tokens that don't exist anymore since it connects a
+        	// loaded token id to stored information about that ID.
             if (tokenForId.containsKey(tokenId)) {
                 tokens.add(tokenForId.get(tokenId));
             }
         }
         return tokens;
     }
- 
+
     /**
      * Given a tag, returns a sorted list of all tokens that have that tag.
      * @param tag The tag to look for.
+     * @return The tokens associated with the requested tag.
      */
-    public List<BaseToken> getTokensForTag(String tag) {
+    public List<BaseToken> getTokensForTag(final String tag) {
         ArrayList<String> tags = new ArrayList<String>();
         tags.add(tag);
         return tokensForTags(tags);
     }
- 
+
     /**
      * Adds a token and tags it with default tags.
-     * TODO: is addToken needed?
      * @param token The token to add and tag.
      */
-    private void addTokenPrototype(BaseToken token) {
-        addToken(token);
+    public void addTokenPrototype(final BaseToken token) {
+        tokenForId.put(token.getTokenId(), token);
         tagToken(token, token.getDefaultTags());
     }
-    
+
     /**
      * Removes the given tag from the given token.  It is not an error to remove
      * a tag that is not on the token in the first place.
      * @param tokenId ID of the token to remove the tag from.
      * @param tag The tag to remove.
      */
-    public void removeTagFromToken(String tokenId, String tag) {
+    public void removeTagFromToken(final String tokenId, final String tag) {
         this.tagsForToken.get(tokenId).remove(tag);
         this.tokensForTag.get(tag).remove(tokenId);
 
     }
-    
+
     /**
      * Removes a token from the database.  This does nothing to ensure that the
      * token will not be re-added next time the database populates.
      * @param token The token to remove.
      */
-    public void removeToken(BaseToken token) {
+    public void removeToken(final BaseToken token) {
         for (String tag : tagsForToken.get(token.getTokenId())) {
             tokensForTag.get(tag).remove(token.getTokenId());
         }
         tagsForToken.remove(token.getTokenId());
         tokenForId.remove(token.getTokenId());
     }
-    
+
     /**
-     * Populates the token database with built-in tokens and custom tokens loaded
-     * from the token manager.
-     * @param dataManager
+     * Populates the token database with built-in tokens and custom tokens
+     * loaded from the token manager.
+     * @param dataManager The data manager to load tokens from.
      */
-    public void populate(DataManager dataManager) {
+    public void populate(final DataManager dataManager) {
         loadCustomImageTokens(dataManager);
         loadBuiltInImageTokens();
         loadColorTokens();
         loadLetterTokens();
     }
-    
+
     /**
      * Searches for tokens that display images from the custom image directory
      * on the SD card and adds them to the database.
      * @param dataManager The data manager to use when searching for tokens.
      */
-    private void loadCustomImageTokens(DataManager dataManager) {
+    private void loadCustomImageTokens(final DataManager dataManager) {
         CustomBitmapToken.registerDataManager(dataManager);
         for (String filename : dataManager.tokenFiles()) {
             addTokenPrototype(new CustomBitmapToken(filename));
@@ -314,8 +303,9 @@ public final class TokenDatabase implements Serializable {
     /**
      * Adds a built-in image token with the given resource ID to the token
      * database.
+     * @param resourceId The ID of the drawable resource to add.
      */
-    private void addBuiltin(int resourceId) {
+    private void addBuiltin(final int resourceId) {
         addTokenPrototype(new BuiltInImageToken(resourceId));
     }
 
@@ -327,7 +317,6 @@ public final class TokenDatabase implements Serializable {
             addTokenPrototype(new SolidColorToken(color));
         }
     }
-
 
     /**
      * Populates the database with letter-in-a-circle tokens.
@@ -366,10 +355,10 @@ public final class TokenDatabase implements Serializable {
      * @param context Context to use when saving the database.
      * @throws IOException on write error.
      */
-    public void save(Context context) throws IOException {
-        FileOutputStream output = 
+    public void save(final Context context) throws IOException {
+        FileOutputStream output =
                 context.openFileOutput("token_database", Context.MODE_PRIVATE);
-        BufferedWriter dataOut = 
+        BufferedWriter dataOut =
                 new BufferedWriter(new OutputStreamWriter(output));
         save(dataOut);
         dataOut.close();
@@ -380,11 +369,11 @@ public final class TokenDatabase implements Serializable {
      * @param output The writer to write the token database to.
      * @throws IOException on write error.
      */
-    private void save(BufferedWriter output) throws IOException {
+    private void save(final BufferedWriter output) throws IOException {
         for (String tokenName : this.tagsForToken.keySet()) {
             output.write(tokenName);
             output.write(FILE_DELIMITER);
-            for(String tag : this.tagsForToken.get(tokenName)) {
+            for (String tag : this.tagsForToken.get(tokenName)) {
                 output.write(tag);
                 output.write(FILE_DELIMITER);
             }
@@ -392,19 +381,33 @@ public final class TokenDatabase implements Serializable {
         }
     }
 
-    public static TokenDatabase load(Context context) throws IOException, ClassNotFoundException {
+    /**
+     * Loads a token database from internal storage, and replaces the current
+     * singleton token database with the loaded one.
+     * @param context The context to use when loading.
+     * @return The loaded database.
+     * @throws IOException On read error.
+     */
+    public static TokenDatabase load(final Context context)
+    		throws IOException {
         TokenDatabase d = new TokenDatabase();
         d.populate(new DataManager(context));
 
         FileInputStream input = context.openFileInput("token_database");
-        BufferedReader dataIn = new BufferedReader(new InputStreamReader(input));
+        BufferedReader dataIn =
+        	new BufferedReader(new InputStreamReader(input));
         d.load(dataIn);
         dataIn.close();
 
         return d;
     }
 
-    private void load(BufferedReader dataIn) throws IOException {
+    /**
+     * Reads data into this token database from the given reader.
+     * @param dataIn Reader to get input from.
+     * @throws IOException On read error.
+     */
+    private void load(final BufferedReader dataIn) throws IOException {
         String line;
         while ((line = dataIn.readLine()) != null) {
             String[] tokens = line.split(FILE_DELIMITER);
