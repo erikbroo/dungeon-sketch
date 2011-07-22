942f96b4137392f43f5eef2a2b75777af1760164
Tim Bocek <tim.bocek@gmail.com>
2011-07-22 22:57:42 +0000

    checkstyle fixes.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@114 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/view/TokenButton.java
+++ src/com/tbocek/android/combatmap/view/TokenButton.java
@@ -1,7 +1,6 @@
 package com.tbocek.android.combatmap.view;
 
 import com.tbocek.android.combatmap.graphicscore.BaseToken;
-import com.tbocek.android.combatmap.graphicscore.PointF;
 
 import android.content.Context;
 import android.graphics.Canvas;
@@ -9,46 +8,75 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.GestureDetector.SimpleOnGestureListener;
-import android.widget.Button;
 import android.widget.ImageView;
 
 /**
- * Represents a button that contains a prototype for a token.  Draws the button based on the token's prototype,
- * and provides a method to construct a copy.
- * @author Tim
+ * Represents a button that contains a prototype for a token.  Draws the button
+ * based on the token's prototype, and provides a method to construct a copy.
+ * @author Tim Bocek
  *
  */
 public final class TokenButton extends ImageView {
-    BaseToken prototype;
+
+	/**
+	 * How much to scale the token by.  1.0 means it is completely inscribed in
+	 * the button element.
+	 */
+	private static final float TOKEN_SCALE = 0.8f;
+
+	/**
+	 * The token represented by this button.
+	 */
+    private BaseToken mPrototype;
+
+    /**
+     * A gesture detector used to detect long presses for drag and drop start.
+     */
     private GestureDetector gestureDetector;
 
-    private SimpleOnGestureListener gestureListener = new SimpleOnGestureListener() {
-        public void onLongPress(MotionEvent e) {
-            //TODO(tbocek): StartDrag
-            startDrag(null, new View.DragShadowBuilder(TokenButton.this), prototype.clone(), 0);
+    /**
+     * A gesture listener used to start a drag and drop when a long press
+     * occurs.
+     */
+    private SimpleOnGestureListener gestureListener =
+    	new SimpleOnGestureListener() {
+        public void onLongPress(final MotionEvent e) {
+            startDrag(null, new View.DragShadowBuilder(TokenButton.this),
+            		  mPrototype.clone(), 0);
         }
     };
 
-    public TokenButton(Context context, BaseToken prototype) {
+    /**
+     * Constructor.
+     * @param context The context to create this view in.
+     * @param prototype The prototype token that this view represents.
+     */
+    public TokenButton(final Context context, final BaseToken prototype) {
         super(context);
-        this.prototype = prototype;
+        this.mPrototype = prototype;
 
         //Set up listener to see if a drag has started.
-        gestureDetector = new GestureDetector(this.getContext(), gestureListener);
-
-        this.prototype.setLocation(new PointF(40, 35));
-    }
-
-    public void onDraw(Canvas c) {
-        prototype.draw(c, (float)this.getWidth()/2, (float)this.getHeight()/2, Math.min(this.getWidth(), this.getHeight()) * .8f / 2);
-    }
-
-    public BaseToken getClone() {
-        return prototype.clone();
+        gestureDetector =
+        	new GestureDetector(this.getContext(), gestureListener);
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent ev) {
+    public void onDraw(final Canvas c) {
+        mPrototype.draw(c, (float) this.getWidth() / 2,
+        		(float) this.getHeight() / 2,
+        		Math.min(this.getWidth(), this.getHeight()) * TOKEN_SCALE / 2);
+    }
+
+    /**
+     * Gets a new token that is a clone of the token specified here.
+     * @return A clone of the token.
+     */
+    public BaseToken getClone() {
+        return mPrototype.clone();
+    }
+
+    @Override
+    public boolean onTouchEvent(final MotionEvent ev) {
         this.gestureDetector.onTouchEvent(ev);
         return super.onTouchEvent(ev);
     }
