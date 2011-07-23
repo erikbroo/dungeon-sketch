aa42914e16d18c18b50fc5c0528ab7a7d8d331d3
Tim Bocek <tim.bocek@gmail.com>
2011-07-23 17:47:25 +0000

    Checkstyle Fixes.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@132 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/view/TokenCategorySelector.java
+++ src/com/tbocek/android/combatmap/view/TokenCategorySelector.java
@@ -1,64 +1,98 @@
 package com.tbocek.android.combatmap.view;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.tbocek.android.combatmap.TokenDatabase;
-
 import android.content.Context;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
-public final class TokenCategorySelector extends ScrollView {
-    private LinearLayout innerLayout;
-    private TokenDatabase tokenDatabase;
-    private OnTagSelectedListener onTagSelectedListener;
+import com.tbocek.android.combatmap.TokenDatabase;
 
+/**
+ * Provides a scrolling view of token categories that can be selected.
+ * @author Tim Bocek
+ *
+ */
+public final class TokenCategorySelector extends ScrollView {
+	/**
+	 * Layout that views will be added to.
+	 */
+    private LinearLayout innerLayout;
+
+    /**
+     * Listener that fires when a tag is selected.
+     */
+    private OnTagSelectedListener mOnTagSelectedListener;
+
+    /**
+     * Size to use when rendering text.
+     */
+    private static final int TEXT_SIZE = 16;
+
+    /**
+     * Sets the listener that fires when a tag is selected.
+     * @param onCheckedListChangedListener The listener to use.
+     */
     public void setOnCheckedListChangedListener(
-            OnTagSelectedListener onCheckedListChangedListener) {
-        this.onTagSelectedListener = onCheckedListChangedListener;
+            final OnTagSelectedListener onCheckedListChangedListener) {
+        this.mOnTagSelectedListener = onCheckedListChangedListener;
     }
 
-    public TokenCategorySelector(Context context) {
+    /**
+     * Constructor.
+     * @param context The context to construct this view in.
+     */
+    public TokenCategorySelector(final Context context) {
         super(context);
         innerLayout = new LinearLayout(this.getContext());
         innerLayout.setOrientation(LinearLayout.VERTICAL);
         this.addView(innerLayout);
-
     }
 
-    private void addCheckbox(String text) {
+
+    /**
+     * Adds a button to the view that will select the given tag.
+     * @param tag The tag represented by this button.
+     */
+    private void addButton(final String tag) {
         Button b = new Button(this.getContext());
-        b.setText(text);
-        b.setTextSize(16);
+        b.setText(tag);
+        b.setTextSize(TEXT_SIZE);
         innerLayout.addView(b);
         b.setOnClickListener(new OnClickListener() {
 
             @Override
-            public void onClick(View v) {
-                if (onTagSelectedListener != null) {
-                    onTagSelectedListener.onTagSelected(((Button)v).getText().toString());
+            public void onClick(final View v) {
+                if (mOnTagSelectedListener != null) {
+                    mOnTagSelectedListener.onTagSelected(
+                    		((Button) v).getText().toString());
                 }
             }
         });
     }
 
-    public void setTokenDatabase(TokenDatabase tokenDatabase) {
-        this.tokenDatabase = tokenDatabase;
+    /**
+     * Reads tags from the token database and sets up this view.
+     * @param tokenDatabase The token database to use tags from.
+     */
+    public void setTokenDatabase(final TokenDatabase tokenDatabase) {
         innerLayout.removeAllViews();
-        for (String tag: tokenDatabase.getTags()) {
-            addCheckbox(tag);
+        for (String tag : tokenDatabase.getTags()) {
+            addButton(tag);
         }
     }
 
-
+    /**
+     * Listener that fires when a new tag is selected.
+     * @author Tim Bocek
+     *
+     */
     public interface OnTagSelectedListener {
-        public void onTagSelected(String checkedTag);
+    	/**
+    	 * Called when a new tag is selected.
+    	 * @param tag The new tag.
+    	 */
+    	void onTagSelected(String tag);
     }
 
 }
