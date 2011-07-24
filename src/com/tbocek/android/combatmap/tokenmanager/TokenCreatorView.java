93ef1ed79468d6c2b560994b437672ef78bbcd0f
Tim Bocek <tim.bocek@gmail.com>
2011-07-24 17:58:08 +0000

    Checkstyle Fixes.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@134 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/tokenmanager/TokenCreatorView.java
+++ src/com/tbocek/android/combatmap/tokenmanager/TokenCreatorView.java
@@ -18,24 +18,67 @@ import android.view.ScaleGestureDetector;
 import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener;
 import android.view.View;
 
+/**
+ * This view is the main rendering and manipulation logic for the token creator
+ * activity.
+ * @author Tim Bocek
+ *
+ */
 public final class TokenCreatorView extends View {
-    private static final int MAX_TOKEN_SIZE=300;
+	/**
+	 * The maximum dimension to output for a square token.  If a token is too
+	 * big it will be scaled down to this when saving.
+	 */
+    private static final int MAX_TOKEN_SIZE = 300;
 
+    /**
+     * Whether a circle has been drawn on the loaded token yet.
+     */
     private boolean hasCircle = false;
 
     // All of the following in screen space
+
+    /**
+     * X coordinate of the center of the circle drawn on the candidate image,
+     * in screen space.
+     */
     private float circleCenterX = 0;
+
+    /**
+     * Y coordinate of the center of the circle drawn on the candidate image,
+     * in screen space.
+     */
     private float circleCenterY = 0;
+
+    /**
+     * Radius of the circle drawn on the candidate image, in screen space.
+     */
     private float circleRadius = 0;
 
+    /**
+     * The current image that is being cropped.
+     */
     private Drawable currentImage = null;
 
+    /**
+     * Detector that will allow the user to move the candidate circle.
+     */
     private GestureDetector gestureDetector;
+
+    /**
+     * Detector that will allow the user to pinch zoom to resize the candidate
+     * circle.
+     */
     private ScaleGestureDetector scaleDetector;
 
+    /**
+     * Gesture listener that moves the candidate circle when the user scrolls.
+     */
     private SimpleOnGestureListener onGesture = new SimpleOnGestureListener() {
         @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        public boolean onScroll(
+        		final MotionEvent e1, final MotionEvent e2,
+        		final float distanceX, final float distanceY) {
             circleCenterX -= distanceX;
             circleCenterY -= distanceY;
             invalidate();
@@ -43,9 +86,14 @@ public final class TokenCreatorView extends View {
         }
     };
 
-    private SimpleOnScaleGestureListener onScaleGesture = new SimpleOnScaleGestureListener() {
+    /**
+     * Gesture listener that resizes the candidate circle when the user pinch
+     * zooms.
+     */
+    private SimpleOnScaleGestureListener onScaleGesture =
+    		new SimpleOnScaleGestureListener() {
         @Override
-        public boolean onScale(ScaleGestureDetector detector) {
+        public boolean onScale(final ScaleGestureDetector detector) {
             circleCenterX = detector.getFocusX();
             circleCenterY = detector.getFocusY();
             circleRadius = detector.getCurrentSpan() / 2;
@@ -55,7 +103,11 @@ public final class TokenCreatorView extends View {
         }
     };
 
-    public TokenCreatorView(Context context) {
+    /**
+     * Constructor.
+     * @param context The context this view is being constructed in.
+     */
+    public TokenCreatorView(final Context context) {
         super(context);
         setFocusable(true);
         setFocusableInTouchMode(true);
@@ -63,23 +115,31 @@ public final class TokenCreatorView extends View {
         scaleDetector = new ScaleGestureDetector(getContext(), onScaleGesture);
     }
 
-    public void setImage(Drawable image) {
+    /**
+     * Sets the candidate image that the user will crop.
+     * @param image The image to eventually use as a token.
+     */
+    public void setImage(final Drawable image) {
         currentImage = image;
+        // New image, so clear the circle.
         hasCircle = false;
         invalidate();
     }
 
 
     @Override
-    public boolean onTouchEvent(MotionEvent ev) {
+    public boolean onTouchEvent(final MotionEvent ev) {
+    	// Forward touch events to the gesture detectors.
         this.gestureDetector.onTouchEvent(ev);
         this.scaleDetector.onTouchEvent(ev);
         return true;
     }
 
     @Override
-    public void onDraw(Canvas canvas) {
-        if (currentImage == null) return;
+    public void onDraw(final Canvas canvas) {
+        if (currentImage == null) {
+        	return;
+        }
 
         currentImage.setBounds(getImageRect());
         if (hasCircle) {
@@ -89,62 +149,78 @@ public final class TokenCreatorView extends View {
             drawHalfBrightnessImage(canvas);
             drawFullBrightnessCircle(canvas);
         } else {
-            // If a circle is being drawn, draw a half-brightness version of
-            // the full image followed by a full-brightness version of the
-            // clipped region
-            drawImage(canvas);
+        	currentImage.draw(canvas);
         }
     }
 
-    public void drawImage(Canvas canvas) {
-        currentImage.draw(canvas);
-    }
-
-    public void drawHalfBrightnessImage(Canvas canvas) {
+    /**
+     * Draws the image at half brightness.
+     * @param canvas The canvas to draw on.
+     */
+    public void drawHalfBrightnessImage(final Canvas canvas) {
         //TODO: Cache the result of the color filter
-        currentImage.setColorFilter( new PorterDuffColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY));
-        drawImage(canvas);
+        currentImage.setColorFilter(
+        		new PorterDuffColorFilter(
+        				Color.GRAY, PorterDuff.Mode.MULTIPLY));
+        currentImage.draw(canvas);
         currentImage.setColorFilter(null);
     }
 
-    public void drawFullBrightnessCircle(Canvas canvas) {
-        //TODO: Set the rect so that we only draw a small region of the screen (not sure if this will actually get a benefit)
+    /**
+     * Draws the full brightness image only in the selected circle.
+     * @param canvas The canvas to draw on.
+     */
+    public void drawFullBrightnessCircle(final Canvas canvas) {
+        //TODO: Set the rect so that we only draw a small region of the screen
+    	// (not sure if this will actually get a benefit)
         canvas.save(Canvas.CLIP_SAVE_FLAG);
         Path p = new Path();
-        p.addCircle(circleCenterX, circleCenterY, circleRadius, Path.Direction.CW);
+        p.addCircle(
+        		circleCenterX, circleCenterY, circleRadius, Path.Direction.CW);
         canvas.clipPath(p);
-        drawImage(canvas);
+        currentImage.draw(canvas);
         canvas.restore();
     }
 
+    /**
+     * Gets a bitmap that has the selected circle inscribed in it.  The circle
+     * is not actually drawn in the bitmap, but is exported such that other
+     * circle drawing methods will draw it correctly.  The bitmap may also be
+     * downscaled if it is large.
+     * @return The bitmap suitable for writing to the token database.
+     */
     public Bitmap getClippedBitmap() {
         //TODO: Downscale in the case of a large image.
-        if (currentImage == null || !hasCircle) return null;
+        if (currentImage == null || !hasCircle) {
+        	return null;
+        }
         float scale = getScaleFactor();
 
         float squareSizeImageSpace = 2 * circleRadius * scale;
-        int bitmapSquareSize = Math.min((int)squareSizeImageSpace, MAX_TOKEN_SIZE);
+        int bitmapSquareSize =
+        	Math.min((int) squareSizeImageSpace, MAX_TOKEN_SIZE);
 
         Bitmap bitmap = Bitmap.createBitmap(
                 bitmapSquareSize, bitmapSquareSize, Bitmap.Config.ARGB_8888);
-        int DEBUG_width = bitmap.getWidth();
-        int DEBUG_height = bitmap.getHeight();
 
-        // Compute a clipping rectangle that is intentionally larger than the bitmap
-        // and allows the bitmap to sit such that the drawn circle is inscribed in it.
+        // Compute a clipping rectangle that is intentionally larger than the
+        // bitmap and allows the bitmap to sit such that the drawn circle is
+        // inscribed in it.
         PointF centerImageSpace = getCenterInImageSpace();
         PointF upperLeftImageSpace = new PointF(
                 centerImageSpace.x - squareSizeImageSpace / 2,
                 centerImageSpace.y - squareSizeImageSpace / 2);
 
         Rect clippingRect = new Rect(
-                (int)(-upperLeftImageSpace.x),
-                (int)(-upperLeftImageSpace.y),
-                (int)(-upperLeftImageSpace.x + currentImage.getIntrinsicWidth()),
-                (int)(-upperLeftImageSpace.y + currentImage.getIntrinsicHeight()));
+                (int) (-upperLeftImageSpace.x),
+                (int) (-upperLeftImageSpace.y),
+                (int) (-upperLeftImageSpace.x
+                		+ currentImage.getIntrinsicWidth()),
+                (int) (-upperLeftImageSpace.y
+                		+ currentImage.getIntrinsicHeight()));
 
         Canvas canvas = new Canvas(bitmap);
-        float outputScale = ((float)bitmapSquareSize)/squareSizeImageSpace;
+        float outputScale = ((float) bitmapSquareSize) / squareSizeImageSpace;
         canvas.scale(outputScale, outputScale);
         currentImage.setBounds(clippingRect);
         currentImage.draw(canvas);
@@ -154,61 +230,80 @@ public final class TokenCreatorView extends View {
 
 
     /**
-     * Returns a rectangle with the same aspect ratio as the image that represents
-     * the area of this view that the image should occupy.
-     * @return
+     * Returns a rectangle with the same aspect ratio as the image that
+     * represents the area of this view that the image should occupy.
+     * @return The rectangle.
      */
     private Rect getImageRect() {
-        if (currentImage == null) return null;
+        if (currentImage == null) {
+        	return null;
+        }
 
-        float screenAspectRatio = ((float)getWidth())/((float)getHeight());
-        float imageAspectRatio = ((float)currentImage.getIntrinsicWidth())/((float)currentImage.getIntrinsicHeight());
+        float screenAspectRatio = ((float) getWidth()) / ((float) getHeight());
+        float imageAspectRatio = ((float) currentImage.getIntrinsicWidth())
+        		/ ((float) currentImage.getIntrinsicHeight());
 
         float width;
         float height;
         if (imageAspectRatio > screenAspectRatio) {
-            // Image is wider than the screen, fit width to screen and center vertically
+            // Image is wider than the screen, fit width to screen and center
+        	// vertically
             width = getWidth();
             height = width / imageAspectRatio;
         } else {
-            // Image is taller than the screen, fit height to screen and center horizontally
+            // Image is taller than the screen, fit height to screen and center
+        	// horizontally
             height = getHeight();
             width = height * imageAspectRatio;
         }
         float startX = (getWidth() - width) / 2;
         float startY = (getHeight() - height) / 2;
-        return new Rect((int)(startX), (int)(startY), (int)(startX + width), (int)(startY + height));
+        return new Rect(
+        		(int) (startX), (int) (startY),
+        		(int) (startX + width), (int) (startY + height));
     }
 
     /**
-     * Returns the ratio of length in image space to length in screen space
-     * @return
+     * Returns the ratio of length in image space to length in screen space.
+     * @return The scale factor.
      */
     private float getScaleFactor() {
-        if (currentImage == null) return 1f;
+        if (currentImage == null) {
+        	return 1f;
+        }
 
-        float screenAspectRatio = ((float)getWidth())/((float)getHeight());
-        float imageAspectRatio = ((float)currentImage.getIntrinsicWidth())/((float)currentImage.getIntrinsicHeight());
+        float screenAspectRatio = ((float) getWidth()) / ((float) getHeight());
+        float imageAspectRatio = ((float) currentImage.getIntrinsicWidth())
+        		/ ((float) currentImage.getIntrinsicHeight());
 
         if (imageAspectRatio > screenAspectRatio) {
             // Screen width is limiting factor
-            return ((float)currentImage.getIntrinsicWidth()) / ((float)getWidth());
+            return ((float) currentImage.getIntrinsicWidth())
+            		/ ((float) getWidth());
         } else {
             // Screen height is limiting factor
-            return ((float)currentImage.getIntrinsicHeight()) / ((float)getHeight());
+            return ((float) currentImage.getIntrinsicHeight())
+            		/ ((float) getHeight());
         }
 
     }
 
+    /**
+     * Transforms the center of the circle in screen space to the center of
+     * the circle in image space.
+     * @return The center in image space.
+     */
     private PointF getCenterInImageSpace() {
-        // First, compute where the center would be if there were no scale factor but the origin
-        // was at the upper left hand corner of the clip rectangle
+        // First, compute where the center would be if there were no scale
+    	// factor but the origin  was at the upper left hand corner of the clip
+    	// rectangle
         Rect clipRect = getImageRect();
         float imageClipSpaceX = circleCenterX - clipRect.left;
         float imageClipSpaceY = circleCenterY - clipRect.top;
 
         // Now, scale these coordinates using the scale factor
         float scaleFactor = getScaleFactor();
-        return new PointF(imageClipSpaceX * scaleFactor, imageClipSpaceY * scaleFactor);
+        return new PointF(
+        		imageClipSpaceX * scaleFactor, imageClipSpaceY * scaleFactor);
     }
 }
