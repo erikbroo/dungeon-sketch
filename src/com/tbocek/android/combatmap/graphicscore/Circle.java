10a42cdcb7839aa763c63455f404eadead2939d5
Tim Bocek <tim.bocek@gmail.com>
2011-09-06 01:04:59 +0000

    Allow erasing parts of a circle.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@206 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/graphicscore/Circle.java
+++ src/com/tbocek/android/combatmap/graphicscore/Circle.java
@@ -16,13 +16,14 @@ public class Circle extends Shape implements Serializable {
 	// In world space.
 	PointF center = null;
 	float radius = Float.NaN;
-	boolean erased = false;
 
 	/**
 	 * A point on the edge of the circle, where the user first placed his
 	 * finger.
 	 */
 	PointF startPoint = null;
+
+	FreehandLine lineForErasing = null;
 
 	public Circle(int color, float newLineStrokeWidth) {
         this.mColor = color;
@@ -37,21 +38,25 @@ public class Circle extends Shape implements Serializable {
 
 	@Override
 	public boolean needsOptimization() {
-		return erased || center == null;
+		return lineForErasing != null;
 	}
 
 	@Override
 	public List<Shape> removeErasedPoints() {
-		List<Shape> shapes = new ArrayList<Shape>();
-		if (!erased && center != null) {
-			shapes.add(this);
-		}
-		erased = false;
-		return shapes;
+		List<Shape> l = new ArrayList<Shape>();
+		l.add(lineForErasing);
+		lineForErasing = null;
+		return l;
 	}
 
 	@Override
 	public void erase(PointF center, float radius) {
+		if (lineForErasing != null) {
+			lineForErasing.erase(center, radius);
+			invalidatePath();
+			return;
+		}
+
 		if (!boundingRectangle.intersectsWithCircle(center, radius)) {
 			return;
 		}
@@ -59,20 +64,34 @@ public class Circle extends Shape implements Serializable {
 		float d = Util.distance(this.center, center);
 
 		if ( d <= radius + this.radius && d >= Math.abs(radius - this.radius)) {
-			erased = true;
+			createLineForErasing();
+			lineForErasing.erase(center, radius);
 			invalidatePath();
+		}
+	}
+
+	private void createLineForErasing() {
+		lineForErasing = new FreehandLine(this.mColor, this.mWidth);
+		for (float rad = 0; rad < 2 * Math.PI; rad += 2 * Math.PI / 64f) {
+			lineForErasing.addPoint(new PointF(
+					(float) center.x + radius * (float) Math.cos(rad),
+					(float) center.y + radius * (float) Math.sin(rad)));
 		}
 	}
 
 	@Override
 	protected Path createPath() {
-		if (erased || center == null || radius == Float.NaN) {
+		if (center == null || radius == Float.NaN) {
 			return null;
 		}
 
-		Path p = new Path();
-		p.addCircle(center.x, center.y, radius, Path.Direction.CW);
-		return p;
+		if (lineForErasing != null) {
+			return lineForErasing.createPath();
+		} else {
+			Path p = new Path();
+			p.addCircle(center.x, center.y, radius, Path.Direction.CW);
+			return p;
+		}
 	}
 
 	@Override
