2c2c322388abae87368b7759ff1d93b1c3d17a63
Tim Bocek <tim.bocek@gmail.com>
2011-09-13 01:40:18 +0000

    Stop drawing a hex grid when it gets too small (Issue 63)
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@224 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/graphicscore/HexGrid.java
+++ src/com/tbocek/android/combatmap/graphicscore/HexGrid.java
@@ -17,6 +17,11 @@ public final class HexGrid extends Grid {
     private static final float TANGENT_30_DEGREES =
         (float) Math.tan(30 * Math.PI / 180);
 
+    /**
+     * Square size at which grid elements will stop being drawn.
+     */
+    private static final float MIN_SQUARE_SIZE = 15;
+
     @Override
     public PointF getNearestSnapPoint(final PointF currentLocation,
             final float tokenDiameter) {
@@ -61,6 +66,11 @@ public final class HexGrid extends Grid {
         int height = canvas.getHeight();
 
         float squareSize = transformer.worldSpaceToScreenSpace(1.0f);
+
+        if (squareSize < MIN_SQUARE_SIZE) {
+        	return;
+        }
+
         float numSquaresHorizontal = (float) width / squareSize;
         float numSquaresVertical =
             (numSquaresHorizontal * ((float) height) / ((float) width));
