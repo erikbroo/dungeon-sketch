c5a98681d114da2062b6c31fb240557ae4cf8015
Tim Bocek <tim.bocek@gmail.com>
2012-01-03 03:15:23 +0000

    About dialog.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@261 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/Settings.java
+++ src/com/tbocek/android/combatmap/Settings.java
@@ -1,6 +1,9 @@
 package com.tbocek.android.combatmap;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 
 /**
@@ -9,9 +12,34 @@ import android.preference.PreferenceActivity;
  *
  */
 public final class Settings extends PreferenceActivity {
+	public static final int DIALOG_ID_ABOUT = 0;
+	
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.layout.settings);
+        
+        // Hook up the about preference
+        Preference dialogPref = (Preference) findPreference("about");
+        dialogPref.setOnPreferenceClickListener(
+        	new OnPreferenceClickListener() {
+
+				@Override
+				public boolean onPreferenceClick(Preference preference) {
+					showDialog(DIALOG_ID_ABOUT);
+					return true;
+				}
+        		
+        	});
+    }
+    
+    @Override
+    public Dialog onCreateDialog(final int id) {
+    	switch(id) {
+    	case DIALOG_ID_ABOUT:
+    		return new AboutDialog(this);
+    	default:
+    		return null;
+    	}
     }
 }
