72968487e21b060ff83ed857e9c4db8c5aa843b1
Tim Bocek <tim.bocek@gmail.com>
2012-01-11 06:20:18 +0000

    Checkstyle Fixes
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@335 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/model/UndoRedoTarget.java
+++ src/com/tbocek/android/combatmap/model/UndoRedoTarget.java
@@ -1,6 +1,19 @@
 package com.tbocek.android.combatmap.model;
 
+/**
+ * Interface for objects that maintain a command history and a position in
+ * that command history, and support undoing and redoing actions.
+ * @author Tim
+ *
+ */
 public interface UndoRedoTarget {
+	/**
+	 * Undoes the action at the current position.
+	 */
 	void undo();
+	
+	/**
+	 * Redoes the action at the current position.
+	 */
 	void redo();
 }
