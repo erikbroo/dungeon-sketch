763f04b0031c537e18671c7ebe23c10419bef7e9
Tim Bocek <tim.bocek@gmail.com>
2012-01-28 05:42:07 +0000

    Can move tokens as a group when multiple tokens are selected.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@425 68e44389-5778-9b72-1722-c5411804bbf0

--- src/com/tbocek/android/combatmap/view/interaction/TokenMultiSelectInteractionMode.java
+++ src/com/tbocek/android/combatmap/view/interaction/TokenMultiSelectInteractionMode.java
@@ -1,8 +1,14 @@
 package com.tbocek.android.combatmap.view.interaction;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import android.graphics.Canvas;
 import android.view.MotionEvent;
 
+import com.google.common.collect.Lists;
 import com.tbocek.android.combatmap.model.primitives.BaseToken;
+import com.tbocek.android.combatmap.model.primitives.CoordinateTransformer;
 import com.tbocek.android.combatmap.model.primitives.PointF;
 import com.tbocek.android.combatmap.view.CombatView;
 
@@ -11,8 +17,24 @@ import com.tbocek.android.combatmap.view.CombatView;
  * @author Tim
  *
  */
-public class TokenMultiSelectInteractionMode extends CombatViewInteractionMode {
+public class TokenMultiSelectInteractionMode extends ZoomPanInteractionMode {
 
+	/**
+	 * Whether the current scroll operation should drag the selected tokens
+	 * or scroll the screen.
+	 */
+	private boolean mDragging;
+
+    /**
+     * Whether the current token has been moved.
+     */
+    private boolean mMoved;
+    
+    /**
+     * Collection of ghost tokens to draw when moving a group of tokens.
+     */
+    private Collection<BaseToken> mUnmovedTokens = Lists.newArrayList();
+	
 	/**
 	 * Constructor.
 	 * @param view The view to manipulate.
@@ -33,4 +55,63 @@ public class TokenMultiSelectInteractionMode extends CombatViewInteractionMode {
     	}
         return true;
     }
+    
+    @Override
+    public boolean onDown(final MotionEvent e) {
+        BaseToken currentToken = getView().getTokens().getTokenUnderPoint(
+        		new PointF(
+        				e.getX(), e.getY()), 
+        				getView().getGridSpaceTransformer());
+
+        if (currentToken != null) {
+        	mMoved = false;
+        	mDragging = true;
+            getView().getTokens().checkpointTokens(new ArrayList<BaseToken>(
+            		getView().getMultiSelect().getSelectedTokens()));
+            for (BaseToken t: getView().getMultiSelect().getSelectedTokens()) {
+    			this.mUnmovedTokens.add(t.clone());
+    		}
+        } else {
+        	mDragging = false;
+        }
+
+        return true;
+    }
+    
+    @Override
+    public void onUp(final MotionEvent ev) {
+    	mUnmovedTokens.clear();
+    	if (mMoved) {
+	    	getView().getTokens().createCommandHistory();
+	    }
+        getView().refreshMap();
+    }
+    
+    @Override
+    public boolean onScroll(final MotionEvent e1, final MotionEvent e2,
+    		final float distanceX, final float distanceY) {
+    	if (mDragging) {
+    		mMoved = true;
+    		CoordinateTransformer transformer 
+    			= getView().getGridSpaceTransformer();
+    		float deltaX = transformer.screenSpaceToWorldSpace(distanceX);
+    		float deltaY = transformer.screenSpaceToWorldSpace(distanceY);
+    		for (BaseToken t: getView().getMultiSelect().getSelectedTokens()) {
+    			t.move(deltaX, deltaY);
+    		}
+    	} else {
+    		return super.onScroll(e1, e2, distanceX, distanceY);
+    	}
+    	getView().refreshMap();
+    	return true;
+    }
+    
+    @Override
+    public void draw(final Canvas c) {
+        for (BaseToken t: mUnmovedTokens) {
+        	t.drawGhost(c, getView().getGridSpaceTransformer(), 
+        			t.getLocation());
+        }
+    }
+    
 }
