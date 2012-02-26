6d7383439764ba74ac9474141ee044ddd155bbd8
Tim Bocek <tim.bocek@gmail.com>
2012-02-26 04:06:15 +0000

    Add some infrastructure for optimizing art credits.  Not finished yet.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@451 68e44389-5778-9b72-1722-c5411804bbf0

---
+++ src/com/tbocek/tools/combatmap/OptimizeArtCredits.java
@@ -0,0 +1,80 @@
+package com.tbocek.tools.combatmap;
+
+import com.tbocek.android.combatmap.R;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * Optimizes art credits file by replacing resource names with resource IDs.
+ * @author Tim
+ *
+ */
+public class OptimizeArtCredits {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			File inputXmlFile = new File(args[1]);
+			File outputXmlFile = new File(args[2]);
+			
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			
+			Document doc = dBuilder.parse(inputXmlFile);
+			
+			walkDom(doc.getDocumentElement());
+			
+			 // Use a Transformer for output
+			 TransformerFactory tFactory =
+			    TransformerFactory.newInstance();
+			 Transformer transformer = tFactory.newTransformer();
+
+			 DOMSource source = new DOMSource(doc);
+			 FileOutputStream out = new FileOutputStream(outputXmlFile);
+			 StreamResult result = new StreamResult(out);
+			 transformer.transform(source, result); 
+			 out.close();
+			
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	/**
+	 * Walks the DOM, replacing node elements with their
+	 * @param element
+	 * @throws NoSuchFieldException 
+	 * @throws IllegalAccessException 
+	 * @throws SecurityException 
+	 * @throws IllegalArgumentException 
+	 */
+	private static void walkDom(Element node) throws IllegalArgumentException, SecurityException, IllegalAccessException, NoSuchFieldException {
+		String res = node.getAttribute("res");
+		if (res != null) {
+			int resourceId = R.drawable.class.getDeclaredField(res).getInt(null);
+			node.setAttribute("res", Integer.toString(resourceId));
+		}
+		
+		NodeList children = node.getChildNodes();
+		for (int i = 0; i < children.getLength(); ++i) {
+			if (children.item(i) instanceof Element) {
+				walkDom((Element) children.item(i));
+			}
+		}
+	}
+}
