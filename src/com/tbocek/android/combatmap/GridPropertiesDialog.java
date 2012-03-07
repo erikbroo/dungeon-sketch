50e5163b4a92b0997fd578afd996fec0e9ffee1b
Tim Bocek <tim.bocek@gmail.com>
2012-03-07 05:00:35 +0000

    add missing file.
    git-svn-id: http://dungeon-sketch.googlecode.com/svn/trunk/dungeon-sketch@457 68e44389-5778-9b72-1722-c5411804bbf0

---
+++ src/com/tbocek/android/combatmap/GridPropertiesDialog.java
@@ -0,0 +1,194 @@
+package com.tbocek.android.combatmap;
+
+import net.margaritov.preference.colorpicker.ColorPickerDialog;
+
+import com.tbocek.android.combatmap.model.GridColorScheme;
+import com.tbocek.android.combatmap.model.HexGridStrategy;
+import com.tbocek.android.combatmap.model.MapData;
+import com.tbocek.android.combatmap.model.RectangularGridStrategy;
+import com.tbocek.android.combatmap.view.ImageToggleButton;
+import com.tbocek.android.combatmap.view.ToggleButtonGroup;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Bitmap.Config;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
+public class GridPropertiesDialog extends Dialog{
+	private ImageButton mForegroundColor;
+	private ImageButton mBackgroundColor;
+	private ImageToggleButton mHexGridButton;
+	private ImageToggleButton mRectGridButton;
+	private Button mThemePresetButton;
+	private MapData mData;
+	private ArrayAdapter<CharSequence> mPresetAdapter;
+	
+	private ToggleButtonGroup mGridTypeToggles = new ToggleButtonGroup();
+	
+	public GridPropertiesDialog(Context context) {
+		super(context);
+		this.setContentView(R.layout.grid_background_properties);
+		mForegroundColor = (ImageButton) this.findViewById(R.id.button_foreground_color);
+		mBackgroundColor = (ImageButton) this.findViewById(R.id.button_background_color);
+		mHexGridButton = (ImageToggleButton) this.findViewById(R.id.button_toggle_hex_grid);
+		mRectGridButton = (ImageToggleButton) this.findViewById(R.id.button_toggle_rect_grid);
+		mGridTypeToggles.add(mHexGridButton);
+		mGridTypeToggles.add(mRectGridButton);
+		
+		mHexGridButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				hexGridButtonClick();
+				
+			}
+
+		});
+		mRectGridButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				rectGridButtonClick();
+			}
+
+		});
+		mForegroundColor.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				foregroundColorClick();
+			}
+			
+		});
+		mBackgroundColor.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				backgroundColorClick();
+			}
+			
+		});
+		mPresetAdapter = ArrayAdapter.createFromResource(
+				this.getContext(), R.array.themeNames, 
+				android.R.layout.simple_spinner_item);
+		mPresetAdapter.setDropDownViewResource(
+				android.R.layout.simple_spinner_dropdown_item);
+		mThemePresetButton = (Button) this.findViewById(
+				R.id.button_theme_presets);
+
+		mThemePresetButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				presetButtonClick();
+			}
+		});
+		
+	}
+	
+	private void presetButtonClick() {
+
+		  new AlertDialog.Builder(this.getContext())
+		  .setTitle("the prompt")
+		  .setAdapter(mPresetAdapter, new DialogInterface.OnClickListener() {
+
+		    @Override
+		    public void onClick(DialogInterface dialog, int which) {
+		    	colorThemeSelected(mPresetAdapter.getItem(which).toString());
+		    	dialog.dismiss();
+		    }
+		  }).create().show();
+	}
+	
+	private void hexGridButtonClick() {
+		mGridTypeToggles.untoggle();
+		mHexGridButton.setToggled(true);
+		mData.getGrid().setDrawStrategy(new HexGridStrategy());
+	}
+	
+	private void rectGridButtonClick() {
+		mGridTypeToggles.untoggle();
+		mRectGridButton.setToggled(true);
+		mData.getGrid().setDrawStrategy(new RectangularGridStrategy());
+	}
+	
+	private void colorThemeSelected(String colorTheme) {
+		GridColorScheme scheme = GridColorScheme.fromNamedScheme(colorTheme);
+		this.mForegroundColor.setImageBitmap(getPreviewBitmap(scheme.getLineColor()));
+		this.mBackgroundColor.setImageBitmap(getPreviewBitmap(scheme.getBackgroundColor()));
+		this.mData.getGrid().setColorScheme(scheme);
+	}
+	
+	ColorPickerDialog picker;
+	
+	void foregroundColorClick() {
+		picker = new ColorPickerDialog(getContext(), mData.getGrid().getColorScheme().getLineColor());
+		picker.setOnColorChangedListener(new ColorPickerDialog.OnColorChangedListener() {
+			
+			@Override
+			public void onColorChanged(int color) {
+				mForegroundColor.setImageBitmap(getPreviewBitmap(color));
+				mData.getGrid().setColorScheme(new GridColorScheme(mData.getGrid().getColorScheme().getBackgroundColor(), color, false));
+				picker.dismiss();
+			}
+		});
+		picker.show();
+	}
+	
+	void backgroundColorClick() {
+		picker = new ColorPickerDialog(getContext(), mData.getGrid().getColorScheme().getBackgroundColor());
+		picker.setOnColorChangedListener(new ColorPickerDialog.OnColorChangedListener() {
+			
+			@Override
+			public void onColorChanged(int color) {
+				mBackgroundColor.setImageBitmap(getPreviewBitmap(color));
+				mData.getGrid().setColorScheme( new GridColorScheme(color, mData.getGrid().getColorScheme().getLineColor(), false));
+				picker.dismiss();
+			}
+		});
+		picker.show();
+	}
+
+	public void setMapData(MapData data) {
+		mBackgroundColor.setImageBitmap(getPreviewBitmap(
+				data.getGrid().getColorScheme().getBackgroundColor()));
+		mForegroundColor.setImageBitmap(getPreviewBitmap(
+				data.getGrid().getColorScheme().getLineColor()));
+		mGridTypeToggles.untoggle();
+		if (data.getGrid().getDrawStrategy() instanceof HexGridStrategy) {
+			mHexGridButton.setToggled(true);
+		} else {
+			mRectGridButton.setToggled(true);
+		}
+		mData = data;
+	}
+	
+	private Bitmap getPreviewBitmap(int color) {
+		float density = getContext().getResources().getDisplayMetrics().density;
+		int d = (int) (density * 31); //30dip
+		Bitmap bm = Bitmap.createBitmap(d, d, Config.ARGB_8888);
+		int w = bm.getWidth();
+		int h = bm.getHeight();
+		int c = color;
+		for (int i = 0; i < w; i++) {
+			for (int j = i; j < h; j++) {
+				c = (i <= 1 || j <= 1 || i >= w-2 || j >= h-2) ? Color.GRAY : color;
+				bm.setPixel(i, j, c);
+				if (i != j) {
+					bm.setPixel(j, i, c);
+				}
+			}
+		}
+
+		return bm;
+	}
+
+}
